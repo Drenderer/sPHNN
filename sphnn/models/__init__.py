f10b0f8a539a2f20d0f5e25734951d54fbb10637
Fabian Roth <roth@cps.tu-darmstadt.de>
2025-10-21 11:31:16 +0200

    Removed files that were moved to the rebuild branch.

--- sphnn/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bphnn import OnsagerNetPotential
-from .neuralode import NODE
-from .normalization import Normalization, NormalizationWrapper
-from .pod_latent_space import PODLatentSpace
-from .stable_neuralode import SNODELyapunov, SNODEProjection, smoothed_relu
